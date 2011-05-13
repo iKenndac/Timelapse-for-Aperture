0b5c852eb596f9f135aac398663480d3f97de5b8
Daniel Kennett <dkennett@Molly.local>
2011-05-13 21:50:06 +0200

    Added readme and licenses.

--- Timelapse For Aperture/H264VideoCompressor.m
+++ Timelapse For Aperture/H264VideoCompressor.m
@@ -3,7 +3,9 @@
 //  Timelapse For Aperture
 //
 //  Created by Daniel Kennett on 5/11/11.
-//  Copyright 2011 KennettNet Software Limited. All rights reserved.
+//  This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License. 
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send 
+//  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 //
 
 #import "H264VideoCompressor.h"
--- Timelapse For Aperture/PreviewGenerator.h
+++ Timelapse For Aperture/PreviewGenerator.h
@@ -3,7 +3,9 @@
 //  Timelapse For Aperture
 //
 //  Created by Daniel Kennett on 10/05/2011.
-//  Copyright 2011 KennettNet Software Limited. All rights reserved.
+//  This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License. 
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send 
+//  a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 //
 
 #import <Foundation/Foundation.h>
